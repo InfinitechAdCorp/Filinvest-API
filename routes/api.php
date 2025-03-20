e47add6a32f0f9e61a7700b15ddecaec52d80142
Infinitech-MosesAlcantara <infinitech.mosesalcantara@gmail.com>
2025-03-20 17:55:17 +0800

    testimonial and faq

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\TypeController;
+use App\Http\Controllers\API\FaqController;
+use App\Http\Controllers\API\TestimonialController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,5 +25,21 @@ Route::prefix('')->group(function () {
         Route::post('', [TypeController::class, 'create']);
         Route::put('', [TypeController::class, 'update']);
         Route::delete('{id}', [TypeController::class, 'delete']);
+    });
+
+    Route::prefix('testimonials')->group(function () {
+        Route::get('', [TestimonialController::class, 'getAll']);
+        Route::get('{id}', [TestimonialController::class, 'get']);
+        Route::post('', [TestimonialController::class, 'create']);
+        Route::put('', [TestimonialController::class, 'update']);
+        Route::delete('{id}', [TestimonialController::class, 'delete']);
+    });
+
+    Route::prefix('faqs')->group(function () {
+        Route::get('', [FaqController::class, 'getAll']);
+        Route::get('{id}', [FaqController::class, 'get']);
+        Route::post('', [FaqController::class, 'create']);
+        Route::put('', [FaqController::class, 'update']);
+        Route::delete('{id}', [FaqController::class, 'delete']);
     });
 });
